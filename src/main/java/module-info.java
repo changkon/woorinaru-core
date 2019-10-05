eafbeb9cc8cacced2cc6e73b0c4d8c97d9d25fdf
Changkon Han <chang_kon@hotmail.com>
2019-10-05 13:51:21 +0900

    Update module-info file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,15 +7,9 @@ module woorinaru.core {
     exports woorinaru.core.dao.spi;
     exports woorinaru.core.service;
     // Exports SPI services
-    uses woorinaru.core.dao.spi.EventDao;
-    uses woorinaru.core.dao.spi.UserDao;
-    uses woorinaru.core.dao.spi.ResourceDao;
-    uses woorinaru.core.dao.spi.TermDao;
-    uses woorinaru.core.dao.spi.WooriClassDao;
-
-    // Service implementations
-//    provides ResourceService with ResourceServiceImpl;
-//    provides TermService with TermServiceImpl;
-//    provides UserService with UserServiceImpl;
-//    provides WooriClassService with WooriClassServiceImpl;
+//    uses woorinaru.core.dao.spi.EventDao;
+//    uses woorinaru.core.dao.spi.UserDao;
+//    uses woorinaru.core.dao.spi.ResourceDao;
+//    uses woorinaru.core.dao.spi.TermDao;
+//    uses woorinaru.core.dao.spi.WooriClassDao;
 }
