4612692b7eb27f8a8bd69c42f3931b41ae5c9c45
Changkon Han <chang_kon@hotmail.com>
2019-10-13 03:07:20 +0900

    Update module-info on removed base package

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,8 @@
 module woorinaru.core {
     // Exports Domain Model classes
-    exports woorinaru.core.model.base;
     exports woorinaru.core.model.management.administration;
     exports woorinaru.core.model.user;
     // Exports SPI and API
     exports woorinaru.core.dao.spi;
     exports woorinaru.core.service;
-    // Exports SPI services
-//    uses woorinaru.core.dao.spi.EventDao;
-//    uses woorinaru.core.dao.spi.UserDao;
-//    uses woorinaru.core.dao.spi.ResourceDao;
-//    uses woorinaru.core.dao.spi.TermDao;
-//    uses woorinaru.core.dao.spi.WooriClassDao;
 }
