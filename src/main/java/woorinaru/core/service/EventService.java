b246c9d89a0c0f6b8ab4c6ce9b747caea65a60ee
Changkon Han <chang_kon@hotmail.com>
2019-11-03 14:57:32 +0900

    Include EventService

---
+++ src/main/java/woorinaru/core/service/EventService.java
@@ -0,0 +1,6 @@
+package woorinaru.core.service;
+
+import woorinaru.core.model.management.administration.Event;
+
+public interface EventService extends GenericService<Event> {
+}
---
+++ src/main/java/woorinaru/core/service/EventServiceImpl.java
@@ -0,0 +1,7 @@
+package woorinaru.core.service;
+
+import woorinaru.core.dao.spi.EventDao;
+import woorinaru.core.model.management.administration.Event;
+
+public class EventServiceImpl extends AbstractGenericService<Event, EventDao> implements EventService {
+}
